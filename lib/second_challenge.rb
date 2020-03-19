9218074b8eb9d4e2966bd177951c42493908d69c
George Chen <georgechen1338@gmail.com>
2020-03-19 03:39:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,18 +2,18 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => {}
-       :matriarch => {}
-       :hero => {}
-       :hero_friends => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
-      
+
     :capulet => {
       #your key/value pairs here!
       :patriarch => {}
-       :matriarch => {}
-       :hero => {}
-       :heroine_friends => {}
+      :matriarch => {}
+      :hero => {}
+      :heroine_friends => []
       }
     }
 end
