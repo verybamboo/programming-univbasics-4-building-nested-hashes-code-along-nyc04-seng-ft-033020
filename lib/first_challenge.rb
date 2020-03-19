fd263d03a923f19dd790920b0a51575bb2bcd406
George Chen <georgechen1338@gmail.com>
2020-03-19 03:33:58 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,12 +1,7 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {
-       :patriarch => {}
-       :matriarch => {}
-       :hero => {}
-       :hero_friends => {}
-     },
-     :capulet => {
-     }
+     :montague => {},
+     
+     :capulet => {}
    }
 end
