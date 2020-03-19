298f4bae80c9a4468f2b3c3c104f527a1e8b3419
George Chen <georgechen1338@gmail.com>
2020-03-19 03:39:40 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -12,7 +12,7 @@ def second_challenge
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
-      :hero => {},
+      :heroine => {},
       :heroine_friends => []
       }
     }
